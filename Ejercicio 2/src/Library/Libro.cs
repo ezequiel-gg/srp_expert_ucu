47f7008e4431a1f616ef8fe49fe3053217f6de2c
ezequiel-gg <54220485+ezequiel-gg@users.noreply.github.com>
2020-09-01 20:27:37 -0300

    wip

--- Ejercicio 2/src/Library/Libro.cs
+++ Ejercicio 2/src/Library/Libro.cs
@@ -6,7 +6,7 @@ namespace SRP
     {
 
         public string Titulo { get ; }  // faltan los setters para encapsularlo
-        public string Autor { get ; } // faltan los setters para encapsularlo
+        public string Autor { get ; } // faltan los setters para encapsularlo, además se podría crear una clase autor para que sea más escalable
         public string Codigo { get ;  } // faltan los setters para encapsularlo
         public string SectorBiblioteca { get ; set; }  // se podria generar otra clase Biblioteca para establecer los sectores y estantes 
         public string EstanteBiblioteca { get ; set; }
